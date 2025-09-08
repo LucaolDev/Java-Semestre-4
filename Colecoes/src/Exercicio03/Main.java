ac7aef506a74a1b3a4ec0d54a88b7abc85978454
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-08 20:28:28 -0300

    ColeÃcoes Exercicio03

---
+++ Colecoes/src/Exercicio03/Main.java
@@ -0,0 +1,32 @@
+package Exercicio03;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Map<Character, Integer> contador = new HashMap<>();
+        String frase;
+
+        System.out.print("Informe uma frase --> ");
+        frase = in.nextLine();
+
+        for(char c : frase.toLowerCase().toCharArray()){
+            if(Character.isLetter(c)){
+//                int aux = contador.get(c);
+                contador.put(c, contador.getOrDefault(c, 0) + 1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry : contador.entrySet()){
+            System.out.println(entry.getKey() + " --> " + entry.getValue());
+        }
+
+        System.out.println();
+        Set<Character> chave = contador.keySet();
+        TreeSet<Character> arvore = new TreeSet<>(chave);
+        for (Character c : arvore){
+            System.out.println(c + " --> " + contador.get(c));
+        }
+    }
+}
