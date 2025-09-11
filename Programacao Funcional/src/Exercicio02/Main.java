8ab9215e8188af1bd9d746673cc88be041829df3
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-10 22:35:22 -0300

    Programacao Funcional Ex01 e Ex02

---
+++ Programacao Funcional/src/Exercicio02/Main.java
@@ -0,0 +1,13 @@
+package Exercicio02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Empregado> lista = new ArrayList<>();
+        lista.add(new Empregado("D", 1500, 15));
+        lista.add(new Empregado("D", 1500, 15));
+        lista.add(new Empregado("D", 1500, 15));
+    }
+}
